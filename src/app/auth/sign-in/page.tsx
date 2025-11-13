d09bab9fd32abb39365af1f299036228b65cc1d4
Mohammed-Saif-Khan <mdsaif4449@gmail.com>
2025-11-13 17:21:53 +0530

    add scroll in login

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SignInForm from "./form";
 import AuthImageView from "../image-view";
+import "@/styles/signup.css";
 
 export default function SignIn() {
   return (
     <div className="grid md:grid-cols-2 h-screen overflow-hidden">
-      <div className="bg-[linear-gradient(134.21deg,_#fff_2.18%,_#efebff_74.65%)] h-full">
+      <div className="bg-[linear-gradient(134.21deg,_#fff_2.18%,_#efebff_74.65%)] h-full overflow-y-auto custom-scrollbar">
         <SignInForm />
       </div>
       <div className="hidden md:grid bg-gradient-to-b from-[#6338f6] to-[#3a2190] h-full">
