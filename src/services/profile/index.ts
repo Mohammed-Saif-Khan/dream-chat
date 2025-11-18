6fcd1cd4def194d64e97752db82e1093457e83ef
Mohammed-Saif-Khan <mdsaif4449@gmail.com>
2025-11-18 22:06:16 +0530

    fix invalid token issue

--- src/services/profile/index.ts
+++ src/services/profile/index.ts
@@ -1,25 +1,19 @@
 import { fetchInstance } from "@/utils/fetch-instance";
+import { nextCookies } from "@/utils/next-cookies";
 import { redirect } from "next/navigation";
 
 export const getProfile = async () => {
-  try {
-    const response = await fetchInstance("api/v1/profile");
-    if (!response.ok) {
-      if (response?.status === 401) {
-        redirect("/auth/sign-in");
-      }
-      const errorText = await response.text();
-      throw new Error(
-        `Failed to fetch getProfile: ${response?.url} ${response.status} ${errorText}`
-      );
+  const response = await fetchInstance("api/v1/profile");
+  if (!response.ok) {
+    if (response?.status === 401) {
+      await nextCookies("token", undefined, "delete");
+      redirect("/auth/sign-in");
     }
-    const result = await response.json();
-
-    return result?.user;
-  } catch (error) {
-    console.log(
-      "getProfile error:",
-      error instanceof Error ? error.message : error
+    const errorText = await response.text();
+    throw new Error(
+      `Failed to fetch getProfile: ${response?.url} ${response.status} ${errorText}`
     );
   }
+  const result = await response.json();
+  return result?.user;
 };
