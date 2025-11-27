20258331b37cd72e4cbbcfcdb237acf2b4895620
Mohammed-Saif-Khan <mdsaif4449@gmail.com>
2025-11-27 16:37:57 +0530

    change the version of next.js 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -32,7 +32,7 @@ import type { NextRequest } from "next/server";
 //   return NextResponse.next();
 // }
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
   const publicPaths = [
