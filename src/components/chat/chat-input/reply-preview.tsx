8f548944e0693916f6701249b9e21143508e5bb6
Mohammed-Saif-Khan <mdsaif4449@gmail.com>
2026-01-03 22:11:33 +0530

    solve reply message truncate issue

--- src/components/chat/chat-input/reply-preview.tsx
+++ src/components/chat/chat-input/reply-preview.tsx
@@ -17,8 +17,8 @@ export default function ReplyPreview({ reply }: { reply: ReplyMessage }) {
       )}
     >
       <div className="w-1 bg-primary shrink-0" />
-      <div className="flex-1 px-3 py-3 min-w-0">
-        <p className="text-sm text-reply-message truncate">
+      <div className="flex-1 px-3 py-3 w-0">
+        <p className="text-sm truncate whitespace-nowrap">
           {reply?.isDelete ? "This message is Deleted" : reply?.message}
         </p>
       </div>
